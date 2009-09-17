e7c08a3acda925b796972be551ec00a33090c502
aster <aster@vers.site>
2009-09-17 20:31:10 +0000

    🔧 Bump pinned Athena revision and align dialect number literal parsing

--- projects/compilers/sxo-dialect-mathematica/src/shared/number_literal.rs
+++ projects/compilers/sxo-dialect-mathematica/src/shared/number_literal.rs
@@ -1,6 +1,7 @@
 //! Frontend number literal parse/render (SXO-owned; Athena holds [`Number`] only).
 
-use athena::{Number, Term, WireNumber, number_from_wire};
+use athena::{Number, Term, numeric::number_from_wire};
+use athena_types::WireNumber;
 
 /// Parse source text into kernel [`Number`] via wire (integer, rational, or machine float).
 pub fn parse_number_literal(text: &str) -> Option<Number> {
@@ -14,8 +15,14 @@ pub fn parse_number_literal(text: &str) -> Option<Number> {
         let n: f64 = source.parse().ok()?;
         Some(Number::machine(n))
     }
+    else if let Some((numer, denom)) = source.split_once('/') {
+        let numer: i64 = numer.trim().parse().ok()?;
+        let denom: i64 = denom.trim().parse().ok()?;
+        let wire = WireNumber::rational_i64(numer, denom).ok()?;
+        number_from_wire(&wire).ok()
+    }
     else {
-        let wire = WireNumber::from_exact_literal(source)?;
+        let wire = WireNumber::from_decimal_str(source)?;
         number_from_wire(&wire).ok()
     }
 }
