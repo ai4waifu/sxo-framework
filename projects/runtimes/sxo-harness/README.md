aef646b46aa533b3c27ec2be9801efe9c51e19c8
aster <aster@vers.site>
2009-11-26 09:53:39 +0000

    ✨ Add shared `@sxo/harness` feature matrix contracts
    Scaffold private @sxo/harness with FeatureCase / FeatureEntry types and optional host / ackend / device fields. Wire the package into workspace uild:ts and 	est:js.

---
+++ projects/runtimes/sxo-harness/README.md
@@ -0,0 +1,10 @@
+# `@sxo/harness`
+
+Private acceptance harness for SXO feature-matrix validation, environment discovery, and reporting.
+
+Not a product runtime. No CLI binary — user commands stay on `@sxo/sxo`.
+
+```bash
+pnpm --filter @sxo/harness build
+pnpm --filter @sxo/harness test
+```
---
+++ projects/runtimes/sxo-harness/src/matrix/types.ts
@@ -0,0 +1,55 @@
+/** Shared Feature Matrix contracts for dialect packages and harness runners. */
+
+export type FeatureStatus = 'supported' | 'partial' | 'unsupported' | 'planned';
+
+export type FeatureEffect = 'pure' | 'stateful' | 'effectful' | 'unevaluated';
+
+export type CaseKind = 'eval' | 'parse' | 'roundtrip' | 'plot' | 'negative' | 'gap';
+
+/** Execution host for a matrix case or report row. */
+export type FeatureHost = 'native' | 'wasm';
+
+/**
+ * Backend identity for acceptance runs.
+ * External reference backends are local opt-in only.
+ */
+export type FeatureBackend =
+    | 'internal-athena'
+    | 'internal-titan'
+    | 'reference-wolfram'
+    | 'reference-matlab'
+    | 'reference-pari-gp';
+
+export type FeatureCase = {
+    id: string;
+    kind: CaseKind;
+    input: string;
+    /** Expected render / substring / marker depending on `kind`. */
+    expected?: string;
+    /** For `negative`: substring that must NOT appear in a successful-looking result. */
+    forbidden?: string;
+    notes?: string;
+    /** Optional host override for this case. */
+    host?: FeatureHost;
+    /** Optional backend override for this case. */
+    backend?: FeatureBackend;
+    /** Optional device tag when `backend` is `internal-titan`. */
+    device?: string;
+};
+
+export type FeatureEntry = {
+    name: string;
+    category: string;
+    status: FeatureStatus;
+    effect: FeatureEffect;
+    notes?: string;
+    cases: readonly FeatureCase[];
+    /** Default host for cases that omit `host`. */
+    host?: FeatureHost;
+    /** Default backend for cases that omit `backend`. */
+    backend?: FeatureBackend;
+    /** Default device for cases that omit `device`. */
+    device?: string;
+};
+
+export type FeatureMatrix = readonly FeatureEntry[];
