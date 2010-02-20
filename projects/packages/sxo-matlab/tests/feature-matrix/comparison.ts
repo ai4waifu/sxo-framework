7d12007b2308ff4f61e6b362169c382f60252fc8
aster <aster@vers.site>
2010-02-20 22:02:24 +0000

    🧪 Promote MATLAB vector comparison masks in the feature matrix
    Record boolean-mask results for elementwise relations instead of `Unequal`/`Greater` head gaps.

--- projects/packages/sxo-matlab/tests/feature-matrix/comparison.ts
+++ projects/packages/sxo-matlab/tests/feature-matrix/comparison.ts
@@ -3,39 +3,41 @@ import { feature } from '@sxo/harness';
 export const comparisonFeatures = [
     feature('eq', 'comparison').supported().pure().eval('eq.true', '3 == 3', 'true').done(),
     feature('ne', 'comparison')
-        .partial('scalar ~= OK; vector [1,2]~=[1,3] stays Unequal head not elementwise mask')
+        .partial('scalar ~= OK; vector Unequal returns boolean mask (not 0/1)')
         .pure()
         .eval('ne.true', '3 ~= 2', 'true')
         .eval('ne.false', '1 ~= 1', 'false')
-        .gap('ne.vec', '[1, 2] ~= [1, 3]', { expected: '[0, 1]', notes: 'currently Unequal([1, 2], [1, 3])' })
+        .eval('ne.vec', '[1, 2] ~= [1, 3]', '[false, true]')
         .done(),
     feature('le', 'comparison')
-        .partial('scalar <= OK; vector / functional le(…) unevaluated')
+        .partial('scalar <= OK; vector mask OK; functional le(…) still open')
         .pure()
         .eval('le.true', '2 <= 3', 'true')
-        .gap('le.vec', '[1, 2] <= [1, 3]', { expected: '[1, 1]' })
+        .eval('le.vec', '[1, 2] <= [1, 3]', '[true, true]')
         .gap('le.fn', 'le(1, 2)', { expected: 'true' })
         .done(),
     feature('ge', 'comparison')
-        .partial('scalar >= OK; vector / functional ge(…) unevaluated')
+        .partial('scalar >= OK; vector mask OK; functional ge(…) still open')
         .pure()
         .eval('ge.true', '1 >= 1', 'true')
         .eval('ge.false', '1 >= 2', 'false')
-        .gap('ge.vec', '[1, 2, 3] >= 2', { expected: '[0, 1, 1]' })
+        .eval('ge.vec', '[1, 2, 3] >= 2', '[false, true, true]')
         .gap('ge.fn', 'ge(2, 2)', { expected: 'true' })
         .done(),
     feature('gt', 'comparison').supported().pure().eval('gt.true', '3 > 2', 'true').done(),
     feature('elementwise_compare', 'comparison')
-        .unsupported('vectorized > / == become Greater/Equal heads, not logical masks')
+        .supported()
         .pure()
-        .gap('gt.vec', '[1, 2, 3] > 2', { expected: '[0, 0, 1]', notes: 'currently Greater([1, 2, 3], 2)' })
+        .notes('vectorized relations return boolean masks rendered as true/false')
+        .eval('gt.vec', '[1, 2, 3] > 2', '[false, false, true]')
         .done(),
     feature('isequal', 'comparison').unsupported().pure().gap('isequal.vec', 'isequal([1, 2], [1, 2])', { expected: '1' }).done(),
     feature('lt_chain', 'comparison')
-        .partial('scalar 1<2<3 via Athena compare chain; elementwise vector Less still open')
+        .supported()
         .pure()
+        .notes('scalar chains and elementwise vector Less')
         .eval('ltchain.123', '1 < 2 < 3', 'true')
-        .gap('ltchain.vec', '[1, 2, 3] < 2', { expected: '[1, 0, 0]' })
+        .eval('ltchain.vec', '[1, 2, 3] < 2', '[true, false, false]')
         .done(),
     feature('eq_fn', 'comparison')
         .unsupported('functional eq([1,2],[1,2]) unevaluated; true==1 stays Equal head')
