73004be5590befc5d698335101bce95c95b3884e
aster <aster@vers.site>
2010-03-14 19:00:53 +0000

    🧪 Lock Mathematica `Cases` with `Blank[Integer]`
    Cover `CollectMatches` for homogeneous and mixed lists in dialect parse tests.

--- projects/dialects/sxo-dialect-mathematica/tests/parse.rs
+++ projects/dialects/sxo-dialect-mathematica/tests/parse.rs
@@ -360,3 +360,10 @@ fn total_sums_list_elements() {
     let h = H::new();
     assert_eq!(h.wolfram(h.eval("Total[{1, 2, 3}]")), "6");
 }
+
+#[test]
+fn cases_filters_integer_blank() {
+    let h = H::new();
+    assert_eq!(h.wolfram(h.eval("Cases[{1, 2, 3}, _Integer]")), "{1, 2, 3}");
+    assert_eq!(h.wolfram(h.eval("Cases[{1, a, 2}, _Integer]")), "{1, 2}");
+}
