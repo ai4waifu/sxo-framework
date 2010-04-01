d11901ae4d83cc8af1ff7bde3b3a298718507e5c
aster <aster@vers.site>
2010-04-01 04:03:59 +0000

    🧪 Promote `While`/`Do` in the Mathematica feature matrix
    Promote control fixtures now wired through loop `ControlPlan` lowering.

--- projects/packages/sxo-mathematica/tests/feature-matrix/control.ts
+++ projects/packages/sxo-mathematica/tests/feature-matrix/control.ts
@@ -3,20 +3,17 @@ import { feature } from '@sxo/harness';
 export const controlFeatures = [
     feature('Catch', 'control').unsupported().pure().gap('catch.throw', 'Catch[Throw[2]]', { expected: '2' }).done(),
     feature('Do', 'control')
-        .unsupported('unevaluated Do[1,{3}] (CountedLoop / Null return not wired for Do head)')
+        .supported()
         .stateful()
-        .gap('do.strip', 'Do[1, {3}]', {
-            expected: 'Null',
-            notes: 'stays Do[1, {3}]; no longer strips to {3}',
-        })
+        .notes('Do lowers to CountedLoop then Null')
+        .eval('do.count', 'Do[1, {3}]', 'Null')
+        .eval('do.binder', 'Do[i, {i, 3}]', 'Null')
         .done(),
     feature('While', 'control')
-        .unsupported('unevaluated While[False,1] (LoopWhile surface not wired for While head)')
+        .supported()
         .stateful()
-        .gap('while.false', 'While[False, 1]', {
-            expected: 'Null',
-            notes: 'stays While[False, 1]; False atom preserved (no longer returns 1)',
-        })
+        .notes('While lowers to LoopWhile')
+        .eval('while.false', 'While[False, 1]', 'Null')
         .done(),
     feature('For', 'control')
         .unsupported('oak error node on For[i=1,i<3,i++,i]')
