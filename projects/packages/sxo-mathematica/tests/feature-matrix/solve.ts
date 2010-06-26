069750e58752e1d9c9e4505a2817700adb11732a
aster <aster@vers.site>
2010-06-26 08:51:19 +0000

    🧪 Promote `Solve` quadratic matrix row
    Mark solve.quad as a supported eval after univariate `Solve` `Goal` coverage.

--- projects/packages/sxo-mathematica/tests/feature-matrix/solve.ts
+++ projects/packages/sxo-mathematica/tests/feature-matrix/solve.ts
@@ -2,10 +2,10 @@ import { feature } from '@sxo/harness';
 
 export const solveFeatures = [
     feature('Solve', 'solve')
-        .partial('Equal residual fixed; univariate SolveGoal / SolutionSet bridge still pending')
+        .supported()
         .pure()
-        .notes('must lower to Athena SolveGoal — not Extension residual')
-        .gap('solve.quad', 'Solve[x^2 == 1, x]', { expected: '{{x -> -1}, {x -> 1}}' })
+        .notes('univariate polynomial equations lower to Athena SolveGoal')
+        .eval('solve.quad', 'Solve[x^2 == 1, x]', '{{x -> -1}, {x -> 1}}')
         .done(),
     feature('NSolve', 'solve').planned().pure().gap('nsolve.quad', 'NSolve[x^2 == 1, x]', { expected: '{{x -> -1.}, {x -> 1.}}' }).done(),
     feature('Reduce', 'solve').planned().pure().gap('reduce.basic', 'Reduce[x^2 > 0, x]', { expected: 'x < 0 || x > 0' }).done(),
