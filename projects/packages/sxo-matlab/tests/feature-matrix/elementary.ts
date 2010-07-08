2bd03f3f97b87c18dc51b7d50ad4fc8e875bf8bb
aster <aster@vers.site>
2010-07-08 15:22:50 +0000

    🧪 Promote MATLAB `exp`/`log` folds and disable pnpm bail
    Athena pin now exact-folds `exp(0)`/`log(1)`, so wrong diagnostics were failing the matrix. Run `test:js` with `--no-bail` so one package failure does not hide the rest.

--- projects/packages/sxo-matlab/tests/feature-matrix/elementary.ts
+++ projects/packages/sxo-matlab/tests/feature-matrix/elementary.ts
@@ -5,24 +5,8 @@ export const elementaryFeatures = [
     feature('cos', 'elementary').supported().pure().eval('cos.0', 'cos(0)', '1').done(),
     feature('sqrt', 'elementary').supported().pure().eval('sqrt.4', 'sqrt(4)', '2').done(),
     feature('abs', 'elementary').supported().pure().eval('abs.neg', 'abs(-3)', '3').done(),
-    feature('exp', 'elementary')
-        .partial('pinned Athena after `fc860cf0` no longer exact-folds `Exp[0]`')
-        .pure()
-        .wrong('exp.0', 'exp(0)', {
-            expected: '1',
-            flags: ['upstream-athena'],
-            notes: 'got unevaluated `exp(0)` on pin `53a7a5dd`',
-        })
-        .done(),
-    feature('log', 'elementary')
-        .partial('pinned Athena after `fc860cf0` no longer exact-folds `Log[1]`')
-        .pure()
-        .wrong('log.1', 'log(1)', {
-            expected: '0',
-            flags: ['upstream-athena'],
-            notes: 'got unevaluated `log(1)` on pin `53a7a5dd`',
-        })
-        .done(),
+    feature('exp', 'elementary').supported().pure().eval('exp.0', 'exp(0)', '1').done(),
+    feature('log', 'elementary').supported().pure().eval('log.1', 'log(1)', '0').done(),
     feature('listable_sin', 'elementary')
         .unsupported('sin on vector left as sin([...])')
         .pure()
