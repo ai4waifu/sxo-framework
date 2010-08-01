598503016cadf58609d4e4c5c8d40691872caaf3
aster <aster@vers.site>
2010-08-01 11:06:50 +0000

    📝 Note Member Form retention for package interop gaps
    Update `containers.Map` / `py.*` / `gpuArray` / `coder` / `matlab.lang` notes: parse keeps `Member`, eval Reject (no longer silent path strip).

--- projects/packages/sxo-matlab/tests/feature-matrix/interop.ts
+++ projects/packages/sxo-matlab/tests/feature-matrix/interop.ts
@@ -2,28 +2,43 @@ import { feature } from '@sxo/harness';
 
 export const interopFeatures = [
     feature('py_list', 'interop')
-        .unsupported('SILENT WRONG: py.list([1,2]) → list([1, 2])')
+        .unsupported('parse keeps Member call path via oak; py.list runtime still open (was silent → list([...]))')
         .pure()
-        .gap('py.list', 'py.list([1, 2])', { expected: '...', notes: 'currently list([1, 2])' })
+        .gap('py.list', 'py.list([1, 2])', {
+            expected: '...',
+            notes: 'Form Application(Member[py, list], …); eval Reject',
+        })
         .done(),
     feature('py_math_sqrt', 'interop')
-        .unsupported('SILENT WRONG: py.math.sqrt(4) and math.sqrt(4) both collapse to matlab sqrt → 2')
+        .unsupported('parse keeps nested Member path; py.math.sqrt runtime still open (was silent → 2)')
         .pure()
-        .gap('py.math.sqrt', 'py.math.sqrt(4)', { expected: '...', notes: 'currently returns 2 via path strip to sqrt' })
+        .gap('py.math.sqrt', 'py.math.sqrt(4)', {
+            expected: '...',
+            notes: 'Form keeps py.math.sqrt chain; eval Reject',
+        })
         .done(),
     feature('gpuArray_zeros', 'interop')
-        .unsupported('SILENT WRONG: gpuArray.zeros(2) → zeros(2) (package path stripped)')
+        .unsupported('parse keeps Member call; gpuArray.zeros runtime still open (was silent → zeros(2))')
         .pure()
-        .gap('gpuarray.zeros', 'gpuArray.zeros(2)', { expected: '...', notes: 'currently zeros(2)' })
+        .gap('gpuarray.zeros', 'gpuArray.zeros(2)', {
+            expected: '...',
+            notes: 'Form Application(Member[gpuArray, zeros], 2); eval Reject',
+        })
         .done(),
     feature('coder_typeof', 'interop')
-        .unsupported('SILENT WRONG: coder.typeof(1) → typeof(1)')
+        .unsupported('parse keeps Member call; coder.typeof runtime still open (was silent → typeof(1))')
         .pure()
-        .gap('coder.typeof', 'coder.typeof(1)', { expected: '...', notes: 'currently typeof(1)' })
+        .gap('coder.typeof', 'coder.typeof(1)', {
+            expected: '...',
+            notes: 'Form Application(Member[coder, typeof], 1); eval Reject',
+        })
         .done(),
     feature('matlab_lang_on', 'interop')
-        .unsupported('SILENT WRONG: matlab.lang.OnOffSwitchState.on → on')
+        .unsupported('parse keeps nested Member path; matlab.lang… runtime still open (was silent → on)')
         .pure()
-        .gap('matlab.lang.on', 'matlab.lang.OnOffSwitchState.on', { expected: '...', notes: 'currently returns on' })
+        .gap('matlab.lang.on', 'matlab.lang.OnOffSwitchState.on', {
+            expected: '...',
+            notes: 'Form Member chain retained; eval Reject',
+        })
         .done(),
 ];
