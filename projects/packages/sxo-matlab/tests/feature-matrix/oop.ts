583becd6b7f36e914f9666150e48ea82de10300a
aster <aster@vers.site>
2010-08-12 02:21:32 +0000

    📝 Retire SILENT WRONG notes for `classdef` and `lsqcurvefit`
    classdef errors instead of bare Foo. lsqcurvefit keeps the full call with Function handle.

--- projects/packages/sxo-matlab/tests/feature-matrix/oop.ts
+++ projects/packages/sxo-matlab/tests/feature-matrix/oop.ts
@@ -2,8 +2,11 @@ import { feature } from '@sxo/harness';
 
 export const oopFeatures = [
     feature('classdef', 'oop')
-        .unsupported('SILENT WRONG: classdef Foo, end → Foo')
+        .unsupported('classdef still unsupported (oak Error / Reject); not silent Foo')
         .stateful()
-        .gap('classdef.foo', 'classdef Foo, end', { expected: '...', notes: 'currently returns Foo' })
+        .gap('classdef.foo', 'classdef Foo, end', {
+            expected: '...',
+            notes: 'must not evaluate to bare Foo; typed Classdef statement is still a follow-up',
+        })
         .done(),
 ];
