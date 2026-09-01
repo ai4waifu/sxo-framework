04d3a9381537bac0222ce0f8d4ed6b412e28381a
aster <aster@vers.site>
2026-09-02 02:47:55 +0800

    🔥 Remove monolithic `sxo-dialects` and retarget workspace default members

--- projects/compilers/sxo-dialects/tests/number_literal.rs
+++
@@ -1,9 +0,0 @@
-//! Integration tests for number_literal.
-
-use sxo_dialects::{Number, parse_number_literal};
-
-#[test]
-fn parse_integer_and_decimal() {
-    assert_eq!(parse_number_literal("42").unwrap(), Number::small_int(42));
-    assert!(parse_number_literal("1.5").unwrap().to_f64_lossy().unwrap() > 1.4);
-}
